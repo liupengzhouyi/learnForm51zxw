db0dc22de4dade47bd90342d393828424f371bc0
liupengzhouyi <37412816+liupengzhouyi@users.noreply.github.com>
2018-06-18 20:56:18 +0800

    Andromeda V 1.0.3

---
+++ src/lp_test_group/DownloadServlet.java
@@ -0,0 +1,44 @@
+package lp_test_group;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@WebServlet(name = "DownloadServlet")
+public class DownloadServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //如何将服务器上的资源返回给浏览器
+        /*//输出字节码文件
+        response.getWriter();*/
+        String filename = request.getParameter("filename");
+        //获取ServletContext对象的就对路径
+        String realPath = this.getServletContext().getRealPath("download/" + filename);
+
+        InputStream inputStream = new FileInputStream(realPath);
+        //输出二进制文件
+        ServletOutputStream outputStream = response.getOutputStream();
+
+        //文件拷贝
+        int len = 0;
+        byte[] buffer = new byte[1024];
+        while((len=inputStream.read(buffer))>0) {
+            outputStream.write(buffer, 0, len);
+        }
+
+        inputStream.close();
+        outputStream.close();
+
+    }
+}
